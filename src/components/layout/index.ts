65c378a75d7f839f4cc2ceb10dc15d62d339708c
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-12-22 17:06:42 +0100

    refactor(components): update barrel exports for Footer and icons
    - Export Footer from layout index
    - Export SocialIcon from ui index

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -3,5 +3,6 @@
  * Contains layout-level components (Header, Footer, etc.)
  */
 
+export { Footer } from './Footer';
 export { Header } from './Header';
 export { MobileMenu } from './MobileMenu';
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -10,3 +10,4 @@ export { BurgerButton } from './BurgerButton';
 export { Button } from './Button';
 export { Logo } from './Logo';
 export { NavLink } from './NavLink';
+export { SocialIcon } from './SocialIcon';
